c4c0e922f79e79fa07ce5d8c0f18020a998706e2
Aleksei Nushtaev <nushtaev2110@gmail.com>
2026-03-10 09:23:26 +0500

    friendly push

--- handlers/handlers_broadcast.py
+++ handlers/handlers_broadcast.py
@@ -147,7 +147,7 @@ async def broadcast_confirm_send(callback: CallbackQuery, state: FSMContext, bot
     # Получаем пользователей по выбранному параметру
     if selected_parameter == "all_users":
         user_ids = await sql.select_all_users()  # Получаем всех пользователей
-        keyboard_broadcast = create_kb(1, r_30old='🔥 30 дней - 99 руб (для своих)')
+        keyboard_broadcast = create_kb(1, ref='👥 Рефералка')
     elif selected_parameter == 'not_connected_subscribe_yes':
         user_ids = await sql.select_not_connected_subscribe_yes()
         keyboard_broadcast = create_kb(1, connect_vpn='🔗 Подключить SpeedGamer')
