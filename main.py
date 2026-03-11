3615d449f816ed6fdf47c22314f125fc39a5753d
Aleksei Nushtaev <nushtaev2110@gmail.com>
2026-03-11 22:10:58 +0500

    add users

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ async def main() -> None:
 
     # Запуск шедулера
     scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
-    # scheduler.add_job(send_message_cron, 'cron', hour=6, minute=15, args=[bot], misfire_grace_time=60)
-    # scheduler.add_job(check_connect, trigger='interval', minutes=14, misfire_grace_time=60)
-    # scheduler.add_job(check_platega, trigger='interval', minutes=1, misfire_grace_time=10)
-    # scheduler.add_job(check_platega_card, trigger='interval', minutes=1, misfire_grace_time=10)
-    # scheduler.add_job(check_platega_crypto, trigger='interval', minutes=1, misfire_grace_time=10)
-    # scheduler.add_job(check_cryptobot_payments, trigger='interval', minutes=1, misfire_grace_time=10)
+    scheduler.add_job(send_message_cron, 'cron', hour=6, minute=15, args=[bot], misfire_grace_time=60)
+    scheduler.add_job(check_connect, trigger='interval', minutes=14, misfire_grace_time=60)
+    scheduler.add_job(check_platega, trigger='interval', minutes=1, misfire_grace_time=10)
+    scheduler.add_job(check_platega_card, trigger='interval', minutes=1, misfire_grace_time=10)
+    scheduler.add_job(check_platega_crypto, trigger='interval', minutes=1, misfire_grace_time=10)
+    scheduler.add_job(check_cryptobot_payments, trigger='interval', minutes=1, misfire_grace_time=10)
     scheduler.add_job(send_push_cron, trigger='interval', minutes=30, misfire_grace_time=60)
     scheduler.add_job(check_online_daily, 'cron', hour=2, minute=55, id='daily_online_stats', misfire_grace_time=60)
     scheduler.start()
